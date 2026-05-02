65f2a766260f4d87a8a8b8d205624feadc66bc39
maio6334 <maio.calabrese@gmail.com>
2026-05-02 18:18:31 +0200

    tested test_code_decode

--- test_hammyng.py
+++ test_hammyng.py
@@ -31,52 +31,65 @@ def test_code_decode():
     '€'  U+020AC  3 byte  b'\xe2\x82\xac'        →  11100010 10000010 10101100
     '𝄞'  U+1D11E  4 byte  b'\xf0\x9d\x84\x9e'   →  11110000 10011101 10000100 10011110 
     """
-    text="A§€𝄞"
+    #text="A§€𝄞"
     #text='la pazza gioia di essere sani'
+    text='''\
+Probabilmente uscì chiudendo dietro a se la porta verde
+Qualcuno si era alzato a preparargli in fretta un caffè d'orzo
+Non so se si girò, non era il tipo d'uomo che si perde'''
+# In nostalgie da ricchi, e andò per la sua strada senza sforzo
+# Quand'io l'ho conosciuto, o inizio a ricordarlo, era già vecchio
+# O così a me sembrava, ma allora non andavo ancora a scuola
+# Colpiva il cranio raso e un misterioso e strano suo apparecchio
+# Un cinto d'ernia che sembrava una fondina per la pistola'''
+
     enc=[]
-    for m in range(len(text)):
-        l=len(text[m].encode())*8
-        print(l)
-        x=ord(text[m])
-        print(x)
-        enc.append(hc.encode(ord(text[m]),l))
+    for i in range(len(text)):
+        l=len(text[i].encode())*8
+        c_ord=ord(text[i])
+        enc.append(hc.encode(c_ord,l))
     print(enc)
     
-    # error routine
-    rate=0.03
-    total_mess=''.join(enc)
-    total_bit=len(total_mess)
-    num_err=int(round(total_bit*rate,0))
+    # inserting errors
+    rate=0.04
+    joined_mess=''.join(enc)
+    total_bits=len(joined_mess)
+    print (f'joined_mess= {joined_mess}\ntotal_bits= {total_bits}')
+    num_err=int(round(total_bits*rate,0))
     split_pos=[0]
     for i in range(len(enc)):
         split_pos.append(split_pos[i]+len(enc[i]))
-    print(f'rate={rate}, total_bit={total_bit},num_err={num_err}')
+    print(f'rate={rate}, total_bits={total_bits},num_err={num_err}')
     rng = np.random.default_rng(12345) # init random generator
-    pos_err = rng.integers(low=0, high=total_bit-1, size=num_err)
-    print(pos_err)
-    # for pos in pos_err:
-    #     print(f'bit flipping position={pos}')
-    #     f= '1' if total_mess[pos]=='0' else '0'
-    #     total_mess= total_mess[:pos] + f+ total_mess[pos+1:]
+    pos_err = rng.integers(low=0, high=total_bits-1, size=num_err)
+    #pos_err=[69,22,78]
+    for pos in pos_err:
+        #print(f'bit flipping position={pos}')
+        f= '1' if joined_mess[pos]=='0' else '0'
+        joined_mess= joined_mess[:pos] + f+ joined_mess[pos+1:]
     ret_mess=[]
-    for i in range(len(split_pos)):
-        to=-1 if i==len(split_pos) else split_pos[i+1]
-        ret_mess.append(total_mess[split_pos[i]:to])
+    for i in range(len(split_pos)-1):
+        ret_mess.append(joined_mess[split_pos[i]:split_pos[i+1]])
     
-    print(enc,ret,sep='\n')
-    exit()
+    print(enc,ret_mess,sep='\n')
+    #try di recover errors
     r=[]
-    for i in range(len(enc)):
-        d = hc.decode(int(enc[i],2), len(enc[i]))
-        r.append(chr(int(d,2)))
+    for i in range(len(ret_mess)):
+        d = hc.decode(int(ret_mess[i],2), len(ret_mess[i]))
+        try:
+            r.append(chr(int(d,2)))
+        except ValueError:
+            print(f'error detecting {i} value, inserting X instead')
+            r.append('X')
     
     r=''.join(r)
     print(r)
+    err=0
     for i in range(len(text)):
         if r[i]!=text[i]:
-            print(f'ko al carattere {i}')
-        
-
+            #print(f'ko al carattere {i}')
+            err+=1
+    print(f'rielvati {err} errori su {len(text)} caratteri')
 def first_test():
     mystr="the mys"
 
